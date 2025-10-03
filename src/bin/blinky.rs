9764d1e69bd0ddd6602600a047cf81370a436421
Piotr Esden-Tempski <piotr@esden.net>
2025-10-02 17:09:51 -0700

    blinky: Updated for v3.0d hardware.

--- src/bin/blinky.rs
+++ src/bin/blinky.rs
@@ -1,9 +1,10 @@
 #![no_std]
 #![no_main]
 
+use blackmagic_rust_firmware::{split_resources, system::{self, AssignedResources, LedResources}};
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_stm32::gpio::{Level, Output, Speed};
+use embassy_stm32::gpio;
 use embassy_time::Timer;
 use {defmt_rtt as _, panic_probe as _};
 
@@ -11,13 +12,24 @@ use {defmt_rtt as _, panic_probe as _};
 async fn main(_spawner: Spawner) -> ! {
     let p = embassy_stm32::init(Default::default());
     info!("Hello World!");
+    let r = split_resources!(p);
 
     // replace PC7 with the right pin for your board.
-    let mut led_r = Output::new(p.PB2, Level::High, Speed::Low);
-    let mut led_o = Output::new(p.PB1, Level::High, Speed::Low);
-    let mut led_y = Output::new(p.PB0, Level::High, Speed::Low);
+    let (mut led_y, mut led_o, mut led_r, mut led_g) = system::get_leds(r.leds);
+
+    led_g.set_high();
+    led_g.set_as_output(gpio::Speed::Low);
 
     loop {
+        defmt::info!("half on!");
+        led_g.set_as_input(gpio::Pull::None);
+        Timer::after_millis(200).await;
+
+        defmt::info!("on!");
+        led_g.set_low();
+        led_g.set_as_output(gpio::Speed::Low);
+        Timer::after_millis(200).await;
+
         defmt::info!("on!");
         led_r.set_low();
         Timer::after_millis(200).await;
@@ -28,6 +40,10 @@ async fn main(_spawner: Spawner) -> ! {
 
         defmt::info!("on!");
         led_y.set_low();
+        Timer::after_millis(200).await;
+
+        defmt::info!("off!");
+        led_g.set_high();
         Timer::after_millis(200).await;
 
         defmt::info!("off!");

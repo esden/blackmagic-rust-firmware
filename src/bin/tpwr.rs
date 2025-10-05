b99f318b53f46d19c6ccb6728f1b74cf965d3ddd
Piotr Esden-Tempski <piotr@esden.net>
2025-10-05 16:23:01 -0700

    tpwr: Converted to v3.0d hardware.

--- src/bin/tpwr.rs
+++ src/bin/tpwr.rs
@@ -1,28 +1,29 @@
 #![no_std]
 #![no_main]
 
+use blackmagic_rust_firmware::{split_resources, system::preamble::*};
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_stm32::gpio::{Level, Output, Speed};
 use embassy_time::Timer;
 use {defmt_rtt as _, panic_probe as _};
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) -> ! {
-    let p = embassy_stm32::init(Default::default());
+    let p = system::init();
+    let r = split_resources!(p);
     info!("Hello World!");
 
-    let mut led_r = Output::new(p.PB2, Level::High, Speed::Low);
-    let mut tpwr_en = Output::new(p.PB12, Level::Low, Speed::Low);
+    let (mut led_y, _, _, _) = system::get_leds(r.leds);
+    let (mut tpwr_en, _, _, _) = system::get_tpwr(r.tpwr);
 
     loop {
         defmt::info!("on!");
-        led_r.set_low();
+        led_y.set_low();
         tpwr_en.set_high();
         Timer::after_secs(5).await;
 
         defmt::info!("off!");
-        led_r.set_high();
+        led_y.set_high();
         tpwr_en.set_low();
         Timer::after_secs(5).await;
     }
